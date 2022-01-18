1ad328a9b6f240763cce0a0fb125196852060c1f
Colby Workman <cgw2319@gmail.com>
2022-01-18 11:00:13 -0700

    routes created for each view

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect, flash
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+
+app.jinja_env.undefined = StrictUndefined
 
 
 @app.route('/')
@@ -9,5 +12,18 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/login')
+def login_page():
+
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register_page():
+
+    return render_template('register.html')
+
 if __name__ == '__main__':
+
+    app.debug = True
     app.run()
